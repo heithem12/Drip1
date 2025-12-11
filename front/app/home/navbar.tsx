4c3593ac18198d74380a68a3154ddfdc40bb51e8
heithem12 <degdeguiheithemheithem12@gmail.com>
2025-12-11 23:17:59 +0100

    third commit

--- front/app/home/navbar.tsx
+++ front/app/home/navbar.tsx
@@ -2,15 +2,17 @@ import Driplogo from "../../public/assets/Driplogo.png"
 import Image from "next/image"
 export default function Navbar(){
     return(
-        <div className="bg-gray 300">
+        <div className="flex justify-center -z-3">
+        <div className=" rounded-xl place-content-center fixed bg-amber-50 inset-shadow-sm ">
             <ul className="ml-20">
-                <Image className="display: inline mr-100" src={Driplogo} alt="logo" width="64" height="64"  />
-                <li className="display: inline p-10">Home</li>
-                <li className="display: inline p-10">Shop</li>
-                <li className="display: inline p-10">Blog</li>
-                <li className="display: inline p-10">Pages</li>
-                <li className="display: inline p-10">Contact</li>
+                <Image className="display: inline  mr-100" src={Driplogo} alt="logo" width="64" height="64"  />
+                <li className="display: inline p-10 cursor-pointer">Home</li>
+                <li className="display: inline p-10 cursor-pointer">Shop</li>
+                <li className="display: inline p-10 cursor-pointer">Blog</li>
+                <li className="display: inline p-10 cursor-pointer">Pages</li>
+                <li className="display: inline p-10 cursor-pointer">Contact</li>
             </ul>
+        </div>
         </div>
     )
 }
--- front/app/home/page.tsx
+++ front/app/home/page.tsx
@@ -1,9 +1,22 @@
 import Navbar from "./navbar";
-
+import Image from 'next/image'
+import mountains from "../../public/assets/images.jpeg"
 export default function Home1(){
     return(
         <div className="">
             <Navbar/>
+            <Image
+      alt="Mountains"
+      src={mountains}
+      placeholder="blur"
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{
+        objectFit: 'cover',
+      }}
+      className="-z-10"
+    />
         </div>
     )
 }
